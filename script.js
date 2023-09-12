5e232bf6d9c60f0264c02a651208c483e58bddb0
Neha kumari <120394661+Nehak90@users.noreply.github.com>
2023-09-12 19:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i= 0; i < str.length()-1; i++) {
+		for (let j = i+1; j < str.length(); j++) {
+			if (str.charAt(i)!=str.charAt(j)) {
+				console.log(str.charAt(i));
+			} else{
+				console.log(null);
+			}
+		}
+		
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
